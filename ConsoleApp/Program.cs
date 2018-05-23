417916f96567065b8fc19dc2dbf0a1ba57a55799
Nick Hamlin <Nick.Hamlin@makingwaves.com>
2018-05-23 17:30:01 -0500

    Added more methods to the Program.cs file

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,24 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using Humanizer;
 using MyLibrary.Extensions;
 
 namespace ConsoleApp
 {
-	class Program
+	static class Program
 	{
-		static void Main()
+		private static void Main()
 		{
-			string number = "28";
-			
-			Console.WriteLine("number.To<int>() => " + number.ConvertTo(-1));
-			Console.WriteLine("number.To<double>() => " + number.ConvertTo(-1D));
-			Console.WriteLine("number.To<decimal>() => " + number.ConvertTo<decimal>(-1));
-			Console.WriteLine("number.To<DateTime>() => " + number.ConvertTo(DateTime.MinValue));
-			Console.WriteLine(new[] {1, 1, 2, 3, 3, 6}.Mode().ToString("|"));
-			Console.WriteLine(new[]{"a", "b", "d", "z"}.ToString(","));
-			Console.WriteLine("Some really really really long line to parse and truncate".Truncate(5, "...", Truncator.FixedNumberOfWords));
+			string number = "27";
+			string reallyLongString = "Some really, really, really long string to parse and truncate.";
+			int[] integers = { 1, 1, 2, 3, 3, 6 };
+			string[] strings = { "a", "b", "d", "z" };
+			char charD = 'a';
+
+			Console.WriteLine((number + ".ConvertTo<int>()").PadRight(50) + " => " + number.ConvertTo(-1));
+			Console.WriteLine((number + ".ConvertTo<double>()").PadRight(50) + " => " + number.ConvertTo(-1D));
+			Console.WriteLine((number + ".ConvertTo<decimal>()").PadRight(50) + " => " + number.ConvertTo<decimal>(-1));
+			Console.WriteLine((number + ".ConvertTo<DateTime>(DateTime.MinValue)").PadRight(50) + " => " + number.ConvertTo(DateTime.MinValue));
+			Console.WriteLine("integers.Mode().ToString(\"|\")".PadRight(50) + " => " + integers.Mode().ToString("|"));
+			Console.WriteLine("strings.ToString(\", \")".PadRight(50) + " => " + strings.ToString(", "));
+			Console.WriteLine(reallyLongString.Truncate(7, "...", Truncator.FixedNumberOfWords));
+			Console.WriteLine(reallyLongString.Truncate(8, "...", Truncator.FixedLength));
+			Console.WriteLine(reallyLongString.Truncate(8, "...", Truncator.FixedNumberOfCharacters));
+			Console.WriteLine((charD + ".ToHex()").PadRight(50) + " => " + 'a'.ToHex());
 			Console.ReadLine();
 		}
 	}
