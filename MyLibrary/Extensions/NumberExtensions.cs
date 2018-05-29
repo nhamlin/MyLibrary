87e9121ccd104f70ddc2aa440ff31617dff6f6a7
Nick Hamlin <Nick.Hamlin@makingwaves.com>
2018-05-29 15:01:09 -0500

    Added additional number extensions

--- MyLibrary/Extensions/NumberExtensions.cs
+++ MyLibrary/Extensions/NumberExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 // ReSharper disable PossibleMultipleEnumeration
@@ -32,6 +33,50 @@ namespace MyLibrary.Extensions
 			return groups
 			       .Where(g => g.Count == maxCount)
 			       .Select(g => g.Value);
+		}
+
+		/// <summary>
+		/// Returns whether the integer is a prime number.
+		/// </summary>
+		/// <param name="input">Integer to check against</param>
+		/// <returns>True/False</returns>
+		public static bool IsPrime(this int input)
+		{
+			if (input < 3)
+				return (input == 2);
+			return Enumerable.Range(2, (int)Math.Sqrt(input)).All(m => input % m != 0);
+		}
+
+		/// <summary>
+		///     Checks if the value is in given range
+		/// </summary>
+		public static bool IsInRange(this int value, int min, int max)
+		{
+			return value >= min && value <= max;
+		}
+
+		/// <summary>
+		///     Checks if the value is in given range
+		/// </summary>
+		public static bool IsInRange(this double value, double min, double max)
+		{
+			return value >= min && value <= max;
+		}
+
+		/// <summary>
+		///     Checks if the value is in given range
+		/// </summary>
+		public static bool IsInRange(this float value, float min, float max)
+		{
+			return value >= min && value <= max;
+		}
+
+		/// <summary>
+		///     Checks if the value is in given range
+		/// </summary>
+		public static bool IsInRange(this decimal value, decimal min, decimal max)
+		{
+			return value >= min && value <= max;
 		}
 	}
 }
