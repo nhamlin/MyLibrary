3f4df412b777eb97158cd489909f084f08c8f700
Nick Hamlin <Nick.Hamlin@makingwaves.com>
2018-07-06 18:07:13 -0500

    Added Url extensions

---
+++ MyLibrary/Extensions/UrlExtensions.cs
@@ -0,0 +1,28 @@
+﻿#region header
+// Copyright (c) 2018
+// Author:         Nicholas Hamlin
+// Created Date:  07/06/2018
+// Filename: MyLibrary:MyLibrary:UrlExtensions.cs
+// Usage:          
+#endregion
+
+using System;
+
+namespace MyLibrary.Extensions
+{
+	/// <summary>
+	/// Extension Methods for URLs
+	/// </summary>
+	public static class UrlExtensions
+	{
+		/// <summary>
+		/// Returns whether the url is null or empty
+		/// </summary>
+		/// <param name="url">Uri to check</param>
+		/// <returns>True/False</returns>
+		public static bool IsNullOrEmpty(this Uri url)
+		{
+			return string.IsNullOrEmpty(url.AbsoluteUri);
+		}
+	}
+}
