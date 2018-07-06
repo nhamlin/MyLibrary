6d61b9701645d13b03a80085c38f46c04d43dcef
Nick Hamlin <Nick.Hamlin@makingwaves.com>
2018-07-06 18:21:51 -0500

    Added IoC container initialization for Episerver

---
+++ EpiServer/SiteBootstrapper.cs
@@ -0,0 +1,21 @@
+﻿#region header
+// Copyright (c) 2018
+// Author:         Nicholas Hamlin
+// Created Date:  07/06/2018
+// Filename: MyLibrary:EpiServer:SiteBootstrapper.cs
+// Usage:          
+#endregion
+
+using StructureMap;
+
+namespace EpiServer
+{
+	public class SiteBootstrapper : Registry
+	{
+		public SiteBootstrapper()
+		{
+			// Add implementations here
+			// TODO: Learn about StructureMap and IoCs
+		}
+	}
+}
---
+++ EpiServer/SiteInitializer.cs
@@ -0,0 +1,38 @@
+﻿#region header
+
+// Copyright (c) 2018
+// Author:         Nicholas Hamlin
+// Created Date:  07/06/2018
+// Filename: MyLibrary:EpiServer:SiteInitializer.cs
+// Usage:          
+
+#endregion
+
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+using InitializationModule = EPiServer.Web.InitializationModule;
+
+namespace EpiServer
+{
+	[InitializableModule]
+	[ModuleDependency(typeof(ServiceContainerInitialization), typeof(InitializationModule))]
+	public class SiteInitializer : IConfigurableModule
+	{
+		public void Initialize(InitializationEngine context)
+		{
+			// Attach event handlers here
+		}
+
+		public void Uninitialize(InitializationEngine context)
+		{
+			// Detach event handlers here
+		}
+
+		public void ConfigureContainer(ServiceConfigurationContext context)
+		{
+			var container = context.StructureMap();
+			container.Configure(creg => creg.AddRegistry<SiteBootstrapper>());
+		}
+	}
+}
