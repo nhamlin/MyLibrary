70554ef6c01102fd940bc3322ac9b44d8efb32e5
Nick Hamlin <>
2018-09-26 11:01:26 -0500

    Added urldecode and urlencode extensions

--- MyLibrary.Core/Extensions/UrlExtensions.cs
+++ MyLibrary.Core/Extensions/UrlExtensions.cs
@@ -7,6 +7,8 @@
 #endregion
 
 using System;
+using System.Text;
+using System.Web;
 
 namespace MyLibrary.Core.Extensions
 {
@@ -23,6 +25,51 @@ namespace MyLibrary.Core.Extensions
 		public static bool IsNullOrEmpty(this Uri url)
 		{
 			return string.IsNullOrEmpty(url.AbsoluteUri);
+		}
+
+		/// <summary>
+		/// Returns a string decoded from a Url
+		/// </summary>
+		/// <param name="source"></param>
+		/// <returns></returns>
+		public static string UrlDecode(this string source)
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			return HttpUtility.UrlDecode(source);
+		}
+
+		/// <summary>
+		/// Returns a string encoded as a Url
+		/// </summary>
+		/// <param name="source"></param>
+		/// <returns></returns>
+		public static string UrlEncode(this string source)
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			if (source.Length <= 32766)
+			{
+				return Uri.EscapeDataString(source);
+			}
+
+			StringBuilder sb = new StringBuilder(source.Length * 2);
+			int startIndex = 0;
+			while (startIndex < source.Length)
+			{
+				int len = Math.Min(source.Length - startIndex, 32766);
+				string stringToEscape = source.Substring(startIndex, len);
+				sb.Append(Uri.EscapeDataString(stringToEscape));
+				startIndex += len;
+			}
+
+			return sb.ToString();
 		}
 	}
 }
