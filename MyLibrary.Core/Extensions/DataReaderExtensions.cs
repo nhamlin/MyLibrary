e79406a925a24f531f74e7909160153faf7cdc2d
Nick Hamlin <nicholas_hamlin@hotmail.com>
2019-04-20 20:05:16 -0500

    Added DataReader extensions & readme file.

--- MyLibrary.Core/Extensions/DataReaderExtensions.cs
+++ MyLibrary.Core/Extensions/DataReaderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿    using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics.Contracts;
 using log4net;
@@ -56,5 +57,161 @@ namespace MyLibrary.Core.Extensions
 				throw new FormatException($"Column doesn't exist: [{column}] for the type [{typeof(T)}]", e);
 			}
 		}
+
+        /// <summary>
+        /// This extends the DataReader.GetBoolean by allowing it to call by field name instead of position.
+        /// </summary>
+        /// <param name="dr"></param>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+	    public static bool GetBoolean(this IDataReader dr, string fieldName)
+	    {
+	        var fieldOrdinal = dr.GetOrdinal(fieldName);
+	        var value = false;
+
+	        if (!dr.IsDBNull(fieldOrdinal))
+            {
+                try
+                {
+                    value = dr.GetBoolean(fieldOrdinal);
+                }
+                catch (InvalidCastException)
+                {
+                    return dr.GetInt16(fieldOrdinal) == 1;
+                }
+	        }
+
+	        return value;
+	    }
+
+        /// <summary>
+        /// This extends the DataReader.GetDateTime by allowing it to call by field name instead of position.
+        /// </summary>
+        /// <param name="dr"></param>
+        /// <param name="fieldName"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+	    public static DateTime GetDateTime(this IDataReader dr, string fieldName, DateTime defaultValue = default(DateTime))
+	    {
+	        var fieldOrdinal = dr.GetOrdinal(fieldName);
+	        return dr.IsDBNull(fieldOrdinal) ? defaultValue : dr.GetDateTime(fieldOrdinal);
+	    }
+
+        /// <summary>
+        /// This extends the DataReader.GetDecimal by allowing it to call by field name instead of position.
+        /// </summary>
+        /// <param name="dr"></param>
+        /// <param name="fieldName"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+	    public static decimal GetDecimal(this IDataReader dr, string fieldName, decimal defaultValue = 0m)
+	    {
+	        var fieldOrdinal = dr.GetOrdinal(fieldName);
+	        return dr.IsDBNull(fieldOrdinal) ? defaultValue : dr.GetDecimal(fieldOrdinal);
+	    }
+
+        /// <summary>
+        /// This extends the DataReader.GetDouble by allowing it to call by field name instead of position.
+        /// </summary>
+        /// <param name="dr"></param>
+        /// <param name="fieldName"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+	    public static double GetDouble(this IDataReader dr, string fieldName, double defaultValue = 0d)
+	    {
+	        var fieldOrdinal = dr.GetOrdinal(fieldName);
+	        return dr.IsDBNull(fieldOrdinal) ? defaultValue : dr.GetDouble(fieldOrdinal);
+	    }
+
+        /// <summary>
+        /// This extends the DataReader.GetFloat by allowing it to call by field name instead of position.
+        /// </summary>
+        /// <param name="dr"></param>
+        /// <param name="fieldName"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+	    public static float GetFloat(this IDataReader dr, string fieldName, float defaultValue = 0f)
+	    {
+	        var fieldOrdinal = dr.GetOrdinal(fieldName);
+	        return dr.IsDBNull(fieldOrdinal) ? defaultValue : dr.GetFloat(fieldOrdinal);
+	    }
+
+        /// <summary>
+        /// This extends the DataReader.GetGuid by allowing it to call by field name instead of position.
+        /// </summary>
+        /// <param name="dr"></param>
+        /// <param name="fieldName"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+	    public static Guid GetGuid(this IDataReader dr, string fieldName, Guid defaultValue = default(Guid))
+	    {
+	        var fieldOrdinal = dr.GetOrdinal(fieldName);
+	        return dr.IsDBNull(fieldOrdinal) ? defaultValue : dr.GetGuid(fieldOrdinal);
+	    }
+
+        /// <summary>
+        /// This extends the DataReader.GetInt16 by allowing it to call by field name instead of position.
+        /// </summary>
+        /// <param name="dr"></param>
+        /// <param name="fieldName"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+	    public static short GetInt16(this IDataReader dr, string fieldName, short defaultValue = 0)
+        {
+            var fieldOrdinal = dr.GetOrdinal(fieldName);
+            return dr.IsDBNull(fieldOrdinal) ? defaultValue : dr.GetInt16(fieldOrdinal);
+        }
+
+        /// <summary>
+        /// This extends the DataReader.GetInt32 by allowing it to call by field name instead of position.
+        /// </summary>
+        /// <param name="dr"></param>
+        /// <param name="fieldName"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+	    public static int GetInt32(this IDataReader dr, string fieldName, int defaultValue = 0)
+	    {
+	        var fieldOrdinal = dr.GetOrdinal(fieldName);
+	        return dr.IsDBNull(fieldOrdinal) ? defaultValue : dr.GetInt32(fieldOrdinal);
+	    }
+
+        /// <summary>
+        /// This extends the DataReader.GetInt64 by allowing it to call by field name instead of position.
+        /// </summary>
+        /// <param name="dr"></param>
+        /// <param name="fieldName"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+	    public static long GetInt64(this IDataReader dr, string fieldName, long defaultValue = 0)
+	    {
+	        var fieldOrdinal = dr.GetOrdinal(fieldName);
+	        return dr.IsDBNull(fieldOrdinal) ? defaultValue : dr.GetInt64(fieldOrdinal);
+	    }
+
+        /// <summary>
+        /// This extends the DataReader.GetString by allowing it to call by field name instead of position. 
+        /// </summary>
+        /// <param name="dr"></param>
+        /// <param name="fieldName"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+	    public static string GetString(this IDataReader dr, string fieldName, string defaultValue = "")
+	    {
+	        var fieldOrdinal = dr.GetOrdinal(fieldName);
+	        return dr.IsDBNull(fieldOrdinal) ? defaultValue : dr.GetString(fieldOrdinal);
+	    }
+
+        /// <summary>
+        /// This allows LINQ operations on a DataReader
+        /// </summary>
+        /// <param name="dr"></param>
+        /// <returns></returns>
+	    public static IEnumerable<IDataRecord> AsEnumerable(this IDataReader dr)
+	    {
+	        while (dr.Read())
+	        {
+	            yield return dr;
+	        }
+	    }
 	}
 }
--- MyLibrary.Core/Extensions/DateReaderExtensions.md
+++ MyLibrary.Core/Extensions/DateReaderExtensions.md
@@ -1,16 +1,132 @@
 ﻿# DateReader Extensions
 
 ```csharp
-
+SqlCommand command = new SqlCommand("SELECT EmployeeID, EmployeeIDGuid, FirstName, LastName, Birthday, Salary, DAYS(TODAY() - StartDate) as DaysEmployed, IsManager FROM Employees;", connection);
+connection.Open()
+var dataReader = command.ExecuteReader();
 ```
 
 ---
-#### 
-
+#### Get&lt;T&gt;(string) : T
+Gets the value of a named column from a DataReader, returning the default(T) if null
 ```csharp
-
+var firstName = dataReader.Get("FirstName");
 ```
 **Output:**  
-_` `_
+_`Mark`_
+
+---
+#### Get&lt;T&gt;(string, T) : T
+Gets the value of a column from a DataReader, returning a specified default value if null, then converts it to T
+```csharp
+string EmployeeId = dataReader.Get<string>("EmployeeID", "Non-Member");
+```
+**Output:**  
+_`"2345"`_
+
+---
+#### GetInt32(string, Int32) : int
+Gets the value of a named column from a DataReader, returning a default value if null
+```csharp
+var EmployeeId = dataReader.GetInt32("EmployeeID");
+```
+**Output:**  
+_`2345`_
+
+---
+#### GetString(string, T) : string
+Gets the value of a column from a DataReader, returning a default value if null
+```csharp
+var firstName = dataReader.GetString("FirstName");
+```
+**Output:**  
+_`Mark`_
+
+---
+#### GetBoolean(string, T) : bool
+Gets the value of a column from a DataReader, returning a default value if null
+```csharp
+var isManager = dataReader.GetBoolean("IsManager");
+```
+**Output:**  
+_`false`_
+
+---
+#### GetDateTime(string, T) : DateTime
+Gets the value of a column from a DataReader, returning a default value if null
+```csharp
+var birthday = dataReader.GetDateTime("Birthday");
+```
+**Output:**  
+_`1991-03-02T00:00:00Z`_
+
+---
+#### GetDecimal(string, T) : decimal
+Gets the value of a column from a DataReader, returning a default value if null
+```csharp
+var salary = dataReader.GetDecimal("Salary");
+```
+**Output:**  
+_`65,960.00`_
+
+---
+#### GetDouble(string, T) : double
+Gets the value of a column from a DataReader, returning a default value if null
+```csharp
+var salary = dataReader.GetDouble("Salary");
+```
+**Output:**  
+_`65,960.0`_
+
+---
+#### GetFloat(string, T) : float
+Gets the value of a column from a DataReader, returning a default value if null
+```csharp
+var salary = dataReader.GetFloat("Salary");
+```
+**Output:**  
+_`65,960.0`_
+
+---
+#### GetGuid(string, T) : Guid
+Gets the value of a column from a DataReader, returning a default value if null
+```csharp
+var employeeIdAsGuid = dataReader.GetDouble("EmployeeIDGuid");
+```
+**Output:**  
+_`1A3B944E-3632-467B-A53A-206305310BAE`_
+
+---
+#### GetInt16(string, T) : short
+Gets the value of a column from a DataReader, returning a default value if null
+```csharp
+var daysEmployed = dataReader.GetDouble("DaysEmployed");
+```
+**Output:**  
+_`220`_
+
+---
+#### GetInt64(string, T) : long
+Gets the value of a column from a DataReader, returning a default value if null
+```csharp
+var daysEmployed = dataReader.GetInt64("DaysEmployed") * 14400;
+```
+**Output:**  
+_`525990`_
+
+---
+#### AsEnumerable() : IEnumerable&lt;IDataRecord&gt;
+Allows for LINQ operations on a DataReader
+```csharp
+var results = dr.AsEnumerable()
+      .Select(record => new Employee {
+            Name = (string)record["FirstName"],
+            Id = (int)record["EmployeeID"],
+            Salary = (decimal)record["Salary"]
+        })
+      .GroupBy(ee => ee.Id);
+```
+**Output:**  
+_`(IEnumerable<IDataRecord>)`_
 
 ---
